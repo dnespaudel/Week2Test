01aa6208771151943c253e4c1ca6eb3b2816bdaa
dnespaudel <dnespaudel10@gmail.com>
2022-03-02 11:34:07 +0545

    Week2Test Completed

---
+++ Query_ValueStocks.sql
@@ -0,0 +1,33 @@
+WITH cte AS(
+SELECT company,
+fiscal_year/10000 as year
+FROM dividend
+), cte2 AS(
+SELECT company,
+(CASE
+	WHEN LEAD(year, 1) OVER (PARTITION BY company ORDER BY year) = year + 1
+	AND LEAD(year, 2) OVER (PARTITION BY company ORDER BY year) = year + 2
+	THEN 'YES'
+	WHEN LAG(year, 1) OVER (PARTITION BY company ORDER BY year) = year - 1
+	AND LAG(year, 2) OVER (PARTITION BY company ORDER BY year) = year - 2
+	THEN 'YES'
+	WHEN LAG(year, 1) OVER (PARTITION BY company ORDER BY year) = year - 1
+	AND LEAD(YEAR, 1) OVER (PARTITION BY company ORDER BY year) = year + 1
+	THEN 'YES'
+	ELSE NULL
+END) AS "value"
+FROM cte
+)
+SELECT distinct company as value_stocks
+FROM cte2
+WHERE
+	value = 'YES';
+
+
+
+
+
+
+
+
+
