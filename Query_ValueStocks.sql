db83bef03a526497f43a91f6e0d63bef70536d3a
dnespaudel <dnespaudel10@gmail.com>
2022-03-05 11:14:33 +0545

    Updated output with array_agg

--- Query_ValueStocks.sql
+++ Query_ValueStocks.sql
@@ -12,14 +12,13 @@ SELECT company,
 	AND LAG(year, 2) OVER (PARTITION BY company ORDER BY year) = year - 2
 	THEN 'YES'
 	WHEN LAG(year, 1) OVER (PARTITION BY company ORDER BY year) = year - 1
-	AND LEAD(YEAR, 1) OVER (PARTITION BY company ORDER BY year) = year + 1
+	AND LEAD(year, 1) OVER (PARTITION BY company ORDER BY year) = year + 1
 	THEN 'YES'
 	ELSE NULL
 END) AS "value"
 FROM cte
 )
-SELECT string_agg(DISTINCT company, ',') as value_stocks
+SELECT array_agg (DISTINCT company || ' ' || '' ) as value_stocks
 FROM cte2
 WHERE
-	value = 'YES'
-;
+	value = 'YES';
